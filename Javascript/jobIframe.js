c439516ad60815957c0f1b47c6cd5807d215a82b
papaPonly <philliplawrenz.riego@gmail.com>
2025-06-02 23:20:03 +0800

    1 2 3 Buttons Working

--- Javascript/jobIframe.js
+++ Javascript/jobIframe.js
@@ -1,3 +1,4 @@
+// For Iframe
 const buttons = document.querySelectorAll('.read-btn');
 const iframe = document.getElementById('jobIframe');
 
@@ -6,4 +7,24 @@ buttons.forEach(button => {
         const url = button.getAttribute('data-url');
         iframe.src = url;
     });
+});
+
+// For Buttons 1 2 3
+document.addEventListener("DOMContentLoaded", () => {
+  const pageButtons = document.querySelectorAll(".page-button");
+  const pages = document.querySelectorAll(".job-page");
+
+  pageButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const targetPage = btn.dataset.page;
+
+      pages.forEach(page => {
+        if (page.dataset.page === targetPage) {
+          page.style.display = "block";
+        } else {
+          page.style.display = "none";
+        }
+      });
+    });
+  });
 });
